57b2df2138c400002dd242be2aa05896ef17e081
iMac Two <jack.green.temp@gmail.com>
2018-07-16 17:17:32 +0800

    使用PyQtGraph绘制折线图 修改数据文件中的格式 删除部分无用代码

--- SensorTool/Main.py
+++ SensorTool/Main.py
@@ -30,6 +30,7 @@ from PyQt5.QtChart import *
 class MainWindow(QMainWindow):
     receiveUpdateSignal = pyqtSignal(str)
     updateChartSignal = pyqtSignal(list)
+    cacheDataSignal = pyqtSignal(list)
     errorSignal = pyqtSignal(str)
     isDetectSerialPort = False
     receiveCount = 0
@@ -40,16 +41,16 @@ class MainWindow(QMainWindow):
     isHideFunctinal = False
     app = None
     dataCache = []
-    dataDeque = deque([])
     offset = None
-    readCount = 0
-    # uartReceiveTimer = QTimer()
     timmer = QTimer()
     feedFlag = True
     fileCache = None
     img = None
-    CHANNELCOUNT = 24
+    CHANNELCOUNT = 32
     dataMin = np.ones(CHANNELCOUNT)*33768
+    TotalSamplesPerChannel = 400 # x轴范围最大值
+    SamplesPerChannel = 16 # 每个通道每次更新的值数量
+    chartData = [[] for i in range(CHANNELCOUNT)]
 
     def __init__(self, app):
         super().__init__()
@@ -65,6 +66,7 @@ class MainWindow(QMainWindow):
         self.initTool()
         self.initEvent()
         self.programStartGetSavedParameters()
+        self.initSim() # 使用模拟数据而不是串口数据 TODO 删除
         return
 
     def __del__(self):
@@ -76,11 +78,12 @@ class MainWindow(QMainWindow):
 
     def initWindow(self):
         QToolTip.setFont(QFont('SansSerif', 10))
-        # main layout
-        mainWidget = QSplitter(Qt.Horizontal)
 
         frameWidget = QWidget()
-        frameLayout = QVBoxLayout() # 整体垂直布局，包括menu和main
+        frameLayout = QVBoxLayout()  # 整体垂直布局，包括menu和main
+
+        # main layout
+        mainWidget = QSplitter(Qt.Horizontal)
 
         configWidget = QWidget()
         configLayout = QVBoxLayout()
@@ -105,12 +108,8 @@ class MainWindow(QMainWindow):
         configLayout.addWidget(self.functionalWiget)
 
         mainLayout.addWidget(self.receiveSendWidget)
-        # mainLayout.addWidget(self.settingWidget)
-        # mainLayout.addWidget(self.functionalWiget)
         mainLayout.addWidget(configWidget)
         mainLayout.setStretch(0, 7)
-        # mainLayout.setStretch(1, 1)
-        # mainLayout.setStretch(2, 1)
 
         menuLayout = QHBoxLayout()
 
@@ -153,32 +152,10 @@ class MainWindow(QMainWindow):
         btnLayout.setStretch(1, 1)
         btnWidget = QWidget()
         btnWidget.setLayout(btnLayout)
-        # self.sendButtion = QPushButton(parameters.strSend)
-        # self.sendHistory = ComboBox()
-        # sendWidget = QWidget()
-        # sendAreaWidgetsLayout = QHBoxLayout()
-        # sendWidget.setLayout(sendAreaWidgetsLayout)
-        # buttonLayout = QVBoxLayout()
-        # buttonLayout.addWidget(self.clearReceiveButtion)
-        # buttonLayout.addStretch(1)
-        # buttonLayout.addWidget(self.sendButtion)
-        self.chartTest = Chart(self.CHANNELCOUNT)
-        self.chartTest.setTitle("实时压力值")
-        self.chartTest.legend().show()
-
-        self.chartView = QChartView(self.chartTest)
-        self.chartView.setStyleSheet("margin: 0px; border: 1px solid black;")
-        # self.chartView.setRubberBand(QChartView.HorizontalRubberBand)
-        # self.chartView.setRubberBand(QChartView.VerticalRubberBand)
-        self.chartView.setRubberBand(QChartView.RectangleRubberBand)
-        self.chartView.setRenderHint(QPainter.Antialiasing)
-        # sendAreaWidgetsLayout.addWidget(self.sendArea)
-        # sendAreaWidgetsLayout.addLayout(buttonLayout)
-        # sendReceiveLayout.addWidget(self.chartView) # TODO 取消注释
 
         # 压力折线图
         self.pw0 = pg.PlotWidget(name='Plot1')
-        # self.pw0.useOpenGL(True)
+        self.pw0.useOpenGL(True)
         # self.pw0.addLegend()
         # self.pw0.setYRange(0, 1536)
         # self.pw0.setBackground('w')
@@ -314,7 +291,9 @@ class MainWindow(QMainWindow):
         self.serialOpenCloseButton.clicked.connect(self.openCloseSerial)
         # self.sendButtion.clicked.connect(self.sendData)
         self.receiveUpdateSignal.connect(self.updateReceivedDataDisplay)
-        self.updateChartSignal.connect(self.chartTest.handleData)
+        # self.updateChartSignal.connect(self.chartTest.handleData) # 使用QChart绘图 TODO 取消注释
+        self.updateChartSignal.connect(self.updateChart)
+        self.cacheDataSignal.connect(self.cacheRawData)
         self.clearReceiveButtion.clicked.connect(self.clearReceiveBuffer)
         self.serialPortCombobox.clicked.connect(self.portComboboxClicked)
         # self.sendSettingsHex.clicked.connect(self.onSendSettingsHexClicked)
@@ -337,6 +316,30 @@ class MainWindow(QMainWindow):
         self.timmer.timeout.connect(self.onTimerOut)
         return
 
+    # 数据模拟发生器
+    def initSim(self):
+
+        self.fileCache = open('cache.txt', 'w')
+        self.fileCache.truncate()
+        self.fileCache = open('cache.txt', 'a')
+
+        self.simTimer = QTimer()
+        self.simTimer.timeout.connect(self.onSimTimerOut)
+        self.simTimer.setInterval(100)
+        self.simTimer.start()
+
+    def onSimTimerOut(self):
+        # print(hex(random.randint(32000, 33000)))
+        strReceived = ''
+        for p in range(self.SamplesPerChannel):
+            for i in range(self.CHANNELCOUNT):
+                # hexStr = hex(random.randint(32000, 33000))
+                hexStr = hex(int(32000 + random.randint(0, 1000) * np.sin(p*2*np.pi/self.SamplesPerChannel + i*2*np.pi/self.CHANNELCOUNT)))
+                # print('%s, %s, %s' % (hexStr, hexStr[2:4], hexStr[4:6]))
+                strReceived += '' + hex(i + 1) + ' ' + hexStr[2:4] + ' ' + hexStr[4:6] + ' '
+        self.receiveUpdateSignal.emit(strReceived)
+
+    # QyQtGraph
     def initPlotChart(self):
         self.curves = []
         # self.texts = []
@@ -352,10 +355,36 @@ class MainWindow(QMainWindow):
             # self.texts.append(text)
 
             self.curves.append(curve)
-        data = np.random.normal(size=(self.CHANNELCOUNT, 400))  # TODO 删除
+        # 曲线数据初始化
+        # data = np.random.normal(size=(self.CHANNELCOUNT, 400))  # TODO 删除
+        # for i in range(self.CHANNELCOUNT):
+        #     temp = (i) % data.shape[0]
+        #     self.curves[i].setData(data[temp])
+
+    def updateChart(self, vals):
+        # print('updateChart')
+        data = np.array(vals) # 24*16
         for i in range(self.CHANNELCOUNT):
-            temp = (i) % data.shape[0]
-            self.curves[i].setData(data[temp])
+            self.chartData[i].append(data[i])
+            if len(self.chartData[i]) > (self.TotalSamplesPerChannel // self.SamplesPerChannel):
+                self.chartData[i].pop(0)
+            temp = np.hstack(self.chartData[i])
+            self.curves[i].setData(temp)
+
+    # 将数据保存到缓存文件
+    def cacheRawData(self, vals):
+        rawData = np.array(vals)
+        dataT = rawData.T  # 16*24
+        temList = [['\r\n'] for i in range(dataT.shape[0])]
+        cStackData = np.column_stack((dataT, np.array(temList)))
+        stackData = np.hstack(cStackData).tolist()
+
+        def myMap(data):
+            if data != '\r\n':
+                return ' ' + str(data) # 不是换行符，加空格
+            return data
+
+        self.cache_save(''.join(map(myMap, stackData)))  # 缓存
 
     def openCloseSerialProcess(self):
         try:
@@ -377,8 +406,8 @@ class MainWindow(QMainWindow):
                 print('接收数据长度 = %d' % (int(len(self.dataCache))))
                 self.dataCache.clear()
                 self.fileCache.close()
+                self.img.clear() # clear压力区域图
                 self.offset = None
-                self.readCount = 0
                 self.timmer.stop()
             else:
                 try:
@@ -451,16 +480,25 @@ class MainWindow(QMainWindow):
         # writer = QTextDocumentWriter(fileName)
         # writer.write(self.receiveArea.document())
         # writer.write(self.fileCache.read())
-        f = open(fileName, 'w')
-        self.fileCache = open('cache.txt', 'r')
-        # QApplication.processEvents()
-        f.write(self.fileCache.read())
-        f.close()
-        self.fileCache = open('cache.txt', 'a')
-        self.serialOpenCloseButton.click()
+        try:
+            f = open(fileName, 'w')
+            self.fileCache = open('cache.txt', 'r')
+            # QApplication.processEvents()
+            f.write(self.fileCache.read())
+            # TODO 根据已选择的通道保存数据到文件
+            f.close()
+            self.fileCache = open('cache.txt', 'a')
+            self.serialOpenCloseButton.click()
+        except Exception as e:
+            print(e)
+
 
     def cache_save(self, datas):
-        self.fileCache.write(datas)
+        try:
+            if self.fileCache is not None and not self.fileCache.closed:
+                self.fileCache.write(datas)
+        except Exception as e:
+            print(e)
 
     def sendData(self):
         try:
@@ -503,7 +541,7 @@ class MainWindow(QMainWindow):
         self.receiveProgressStop = False
         while(not self.receiveProgressStop):
             try:
-                length = 3*16
+                length = 3 * self.SamplesPerChannel
                 bytes = self.com.read(length)
                 # print('length = %s, len(bytes)= %s' % (length, len(bytes)))
                 self.receiveCount += len(bytes)
@@ -553,14 +591,15 @@ class MainWindow(QMainWindow):
         return
 
     def onTimerOut(self):
-        samples = 16 # 每次每个通道更新16个数据点，16 = 400/(1000/40)，其中400指每个通道收到数据点的速度是400pts（串口接收速率）
+        samples = self.SamplesPerChannel # 每次每个通道更新16个数据点，16 = 400/(1000/40)，其中400指每个通道收到数据点的速度是400pts（串口接收速率）
         if len(self.dataCache) > (3 * samples * self.CHANNELCOUNT):
 
-            self.cache_save(' '.join(self.dataCache[0:3 * samples * self.CHANNELCOUNT]))  # 缓存
+            # self.cache_save(' '.join(self.dataCache[0:3 * samples * self.CHANNELCOUNT]))  # 缓存
 
             QApplication.processEvents()
 
             toShowData = [[] for i in range(self.CHANNELCOUNT)]
+            rawData = [[] for i in range(self.CHANNELCOUNT)]
 
             for i in range(samples * self.CHANNELCOUNT):
                 channelNumber = int(self.dataCache[0], 16)
@@ -568,7 +607,8 @@ class MainWindow(QMainWindow):
                 # toShowData[channelNumber - 1].append(channelData)
                 if np.array(channelData).min() < self.dataMin[channelNumber - 1]:
                     self.dataMin[channelNumber - 1] = np.array(channelData).min()
-                toShowData[channelNumber - 1].append(channelData - self.dataMin[channelNumber - 1])
+                toShowData[channelNumber - 1].append(channelData - self.dataMin[channelNumber - 1]) # toShowData维度：(24, 16)
+                rawData[channelNumber - 1].append(channelData) # rowData维度：(24, 16)
                 # print('%s, %s' % (channelNumber, channelData))
                 # print(toShowData)
                 del self.dataCache[0:3]
@@ -576,7 +616,8 @@ class MainWindow(QMainWindow):
                 self.feedFlag = False
                 start = time.clock()
 
-                self.updateChartSignal.emit(toShowData)  # 数据发送给折线图
+                self.updateChartSignal.emit(toShowData)
+                self.cacheDataSignal.emit(rawData)
 
                 tempA = np.array(toShowData)  # list 转 array
                 tempB = tempA[:, 0]  # 取array的第一列数据用于显示压力区域，该array维度为(24,1)
@@ -592,7 +633,7 @@ class MainWindow(QMainWindow):
                 print("Time used: %.3fs" % elapsed)
             except Exception as e:
                 print("chart.handleData error: %s" % e)
-            print(len(self.dataCache))
+            # print(len(self.dataCache))
 
     def blend_color(self, color1, color2, f):
         [r1, g1, b1] = color1
@@ -627,14 +668,14 @@ class MainWindow(QMainWindow):
 
     def clearReceiveBuffer(self):
         self.dataCache.clear() # 清空数据缓存list
-        self.readCount = 0 # 清空读取数据缓存次数的计数器
         self.offset = None
         # self.receiveArea.clear()
         self.receiveCount = 0
         self.sendCount = 0
         self.receiveUpdateSignal.emit(None)
         # 清空图表
-        self.chartTest.handleClear()
+        # self.chartTest.handleClear()
+        self.chartData = [[] for i in range(self.CHANNELCOUNT)]
         return
 
     def MoveToCenter(self):
@@ -806,7 +847,13 @@ class MainWindow(QMainWindow):
     def functionSetVisible(self):
         # self.chartTest.setVisibleFlag(1, self.__getattribute__('ChannelCheckBox1').isChecked())
         for channelNum in range(self.CHANNELCOUNT):
-            self.chartTest.setVisibleFlag((channelNum + 1), self.__getattribute__("ChannelCheckBox" + str(channelNum + 1)).isChecked())
+            # self.chartTest.setVisibleFlag((channelNum + 1), self.__getattribute__("ChannelCheckBox" + str(channelNum + 1)).isChecked())
+
+            # PyQtGraph
+            if self.__getattribute__("ChannelCheckBox" + str(channelNum + 1)).isChecked():
+                self.curves[channelNum].show()
+            else:
+                self.curves[channelNum].hide()
 
     def showHideSettings(self):
         if self.isHideSettings:
@@ -874,141 +921,6 @@ class MainWindow(QMainWindow):
 
     def openDevManagement(self):
         os.system('start devmgmt.msc')
-
-class Chart(QChart):
-
-    def __init__(self, channelCount):
-        super().__init__()
-        self.m_series = 0
-        # self.m_axis = QValueAxis()
-        self.xMax = 400
-        self.yMin = 0
-        self.yMax = 1536
-        self.offset = 0
-        self.channelNumber = 8
-        self.channelCount = channelCount
-        self.channelColor = [
-            Qt.red, Qt.yellow, Qt.green, Qt.blue, Qt.black, Qt.gray, Qt.magenta, Qt.cyan,
-            Qt.darkRed, Qt.darkYellow, Qt.darkGreen, Qt.darkBlue, Qt.black, Qt.darkGray, Qt.darkMagenta, Qt.darkCyan,
-            QColor('#EC7063'), QColor('#F4D03F'), QColor('#58D68D'), QColor('#5499C7'), QColor('#5D6D7E'), QColor('#CACFD2'), QColor('#BA4A00'), QColor('#117A65')]
-        self.seriesList = []
-
-        pen = []
-        self.visibleFlag = [] # 是否显示的标志位
-
-        # pen = QPen(Qt.red)
-        # pen = QPen(QColor('#409EFF'))
-        # pen.setWidth(1)
-
-        p1 = QPen(Qt.red)
-        p1.setWidth(1)
-
-        # self.m_series = QLineSeries()
-        # self.m_series.setPen(pen)
-
-        self.scatseries0 = QScatterSeries()
-        self.scatseries0.setMarkerShape(QScatterSeries.MarkerShapeCircle)  # 设置点的类型
-        self.scatseries0.setMarkerSize(2)  # 设置点的大小
-        self.scatseries0.setPen(p1)
-
-        # self.addSeries(self.m_series)
-        # self.addSeries(self.scatseries0)
-
-        self.axisX = QValueAxis()
-        self.axisX.setRange(0, self.xMax)
-        self.axisX.setTitleText('x值')
-
-        self.axisY = QValueAxis()
-        self.axisY.setRange(self.yMin, self.yMax)
-        self.axisY.setTitleText('Y值')
-
-        self.addAxis(self.axisX, Qt.AlignBottom)
-        self.addAxis(self.axisY, Qt.AlignLeft)
-
-        # self.m_series.attachAxis(self.axisX)
-        # self.m_series.attachAxis(self.axisY)
-        # self.scatseries0.attachAxis(self.axisX)
-        # self.scatseries0.attachAxis(self.axisY)
-
-        for num in range(self.channelCount):
-            # print(num)
-            # pen[num] = QPen(self.channelColor[num])
-            pen.append(QPen(QColor(random.random()*255, random.random()*255, random.random()*255,)))
-            self.visibleFlag.append(True)
-            # print(len(pen))
-            pen[num].setWidth(1)
-            self.seriesList.append(QLineSeries())
-            self.seriesList[num].setName('CH'+str(num + 1))
-            self.seriesList[num].setPen(pen[num])
-            self.seriesList[num].setUseOpenGL(True)
-            self.addSeries(self.seriesList[num])
-            self.seriesList[num].attachAxis(self.axisX)
-            self.seriesList[num].attachAxis(self.axisY)
-
-        # self.createDefaultAxes()
-        # self.setAxisX(self.m_axis, self.m_series)
-        # self.m_axis.setTickCount(5)
-        # self.axisX().setRange(0, self.xMax)
-        # self.axisY().setRange(self.yMin, self.yMax)
-
-    def handleData(self, vals):
-        for num in range(self.channelCount):
-            if self.offset < self.xMax or len(self.seriesList[num].pointsVector()) < self.xMax:
-                # self.seriesList[num].append(self.offset, val)
-                points = self.seriesList[num].pointsVector()
-                for x in range(len(vals[num])):
-                    points.append(QPoint(self.offset + x, vals[num][x]))
-                self.seriesList[num].replace(points)
-            else:
-                # print("min = %d, max = %d" % (self.axisX.min(), self.axisX.max()))
-                self.xMax = self.axisX.max() - self.axisX.min()
-                # print("xMax = %d" % self.xMax)
-                points = self.seriesList[num].pointsVector()
-                # 把数据前移一个单位
-                for index in range(len(points) - len(vals[num])):
-                    # points[index] = QPoint(self.offset - self.xMax + index, points[index + len(vals[num])].y())  # 与后面setRange配合，完成滚动显示
-                    points[index].setY(points[index + len(vals[num])].y())
-                for i in range(len(vals[num])):
-                    # points[len(points) - len(vals[num]) + i] = QPoint(self.offset - self.xMax + len(points) - len(vals[num]) + i + 1, vals[num][i])
-                    points[len(points) - len(vals[num]) + i].setY(vals[num][i])
-                self.seriesList[num].replace(points)
-                # self.axisX.setRange(self.offset - self.xMax, self.offset)  # 修改x轴显示范围
-        self.offset += len(vals[0])
-
-    def handleClear(self):
-        self.offset = 0
-        # self.yMin = 37000
-        # self.yMax = 32768
-        # self.xMax = 100
-        # self.axisX.setRange(0, self.xMax)
-        for num in range(self.channelCount):
-            self.seriesList[num].clear()
-
-    def grabMouseEvent(self, *args, **kwargs):
-        self.xMax = self.xMax * 2
-
-    def setVisibleFlag(self, channel, val):
-        # print(channel)
-        # print(val)
-        self.visibleFlag[channel-1] = val
-        self.seriesList[channel-1].setVisible(val)
-        # print("%d %s" % (channel, self.visibleFlag[channel-1]))
-
-    def handleTimeout(self):
-        x = self.plotArea().width() / self.m_axis.tickCount()
-        y = (self.m_axis.max() - self.m_axis.min()) / self.m_axis.tickCount()
-        self.m_x += y
-        self.axisX().setRange(0, self.m_x+10)
-        self.m_y += random.random()*100
-        self.axisY().setRange(0, self.m_y+5)
-        # print(self.m_x)
-        # print(self.m_y)
-        self.m_series.append(self.m_x, self.m_y)
-        self.scroll(x, 0)
-        # if (self.m_x == 1000):
-        #     self.m_timer.stop()
-
-
 
 def main():
     app = QApplication(sys.argv)
